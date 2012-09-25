f764bb3c1ad3156f1a5624d14b01e5f7d5cc168f
jeff <jefff35@gmail.com>
2012-09-25 19:09:34 +0200

    initializing rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MindOverflow::Application.config.secret_token = 'c678be4e99b6e1e9283aaff9ca793f142ea983f0cd47e0ad715e2e46b63e68b12bac65ba672b7570977842cead5fb0e5f18970f2465731064859a9e7af1e01f3'
