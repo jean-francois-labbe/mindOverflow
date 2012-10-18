c803ffd6ca54a2d32390e38d859786969c777eee
jeff <jefff35@gmail.com>
2012-10-18 19:42:00 +0200

    Added atom feed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @articles }
+      format.atom
     end
   end
 
