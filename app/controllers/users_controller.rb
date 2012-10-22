5ab4b63627d3b9a410259b0a5e6efc106b37044f
jeff <jefff35@gmail.com>
2012-10-22 22:03:28 +0200

    added list of user posts in show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @articles = @user.articles.paginate(:page => params[:page])
 
     respond_to do |format|
       format.html # show.html.erb
