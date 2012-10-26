2630bac3c6a19e9a90153157df3f15b00041ca94
jeff <jefff35@gmail.com>
2012-10-26 13:18:43 +0200

    removed unused includes

--- Gemfile
+++ Gemfile
@@ -22,6 +22,10 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+group :test do
+  gem 'faker'
+end
+
 gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
@@ -51,6 +55,8 @@ group :development do
   gem 'sextant'
   gem "rails_best_practices"
   gem 'rack-mini-profiler'
+  gem 'letter_opener'
+  gem 'pry'
 end
 
 
@@ -63,5 +69,5 @@ gem 'acts_as_favorite'
 gem 'paperclip'
 
 gem 'cancan'
-gem "jquery-rails"
+gem 'jquery-rails'
 gem 'rails3-jquery-autocomplete'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+User.delete_all
+user = User.new(:email => "jle@kereval.com",  :password => '1234', :nickname => "jeff")
+user.save!(:validate => false)
+
+user1 = User.new(:email => "titi@toto.com",  :password => '1234', :nickname => "titi")
+user1.save!(:validate => false)
+
+user2 = User.new(:email => "toto@gmail.com",  :password => 'azerty', :nickname => 'toto')
+user2.save!(:validate => false)
+
+
+Article.delete_all
+10000.times do
+  title = Faker::Lorem.sentence(5)
+  body = Faker::Lorem.paragraphs
+  if u = user1
+    u = user2
+  else
+    u = user1
+  end
+  art = Article.create(:title => title ,:body => body,:user_id => u.id)
+end
