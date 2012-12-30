461cd8692bf78a4f136b6f8bfc0736de9a3bac20
jeff <jefff35@gmail.com>
2012-12-30 01:56:28 +0100

    application helpers to know which controller is executed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?d=identicon&s=#{size}"
   end
+
+  def tab_class(tab_key)
+    active = case tab_key
+    when :articles then controller.controller_name == "articles"
+    when :questions then controller.controller_name == "questions"
+    else
+      false
+    end
+    active ? "current" : nil
+  end
 end
